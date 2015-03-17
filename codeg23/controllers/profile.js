41c31f00233244bab4a7edd2372900968b96bfc9
Yanjia Li <liyanjia92@gmail.com>
2015-03-17 19:18:54 -0400

    Add placeholder

--- codeg23/controllers/profile.js
+++ codeg23/controllers/profile.js
@@ -5,7 +5,7 @@ var UserSchema = require('../models/user');
 /* Get profile page */
 router.get('/:id', function(req, res) {
 	var idString = req.params.id;
-	UserSchema.find({},function(err,user){
+	UserSchema.find({id:idString},function(err,user){
 		UserSchema.find({},function(err, user){
 			console.log(user);
 			if(err) throw err;
