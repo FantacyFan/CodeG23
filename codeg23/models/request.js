71bdd2ed2fcfe4555c65c88e35601a8dbcb7f7c0
FantacyFan <Benran@dhcp-rhodes-641.eduroam.cornell.edu>
2015-04-04 16:12:26 -0400

    change index video background to cover and add style for posts and requests

--- codeg23/models/request.js
+++ codeg23/models/request.js
@@ -8,7 +8,7 @@ var requestSchema = mongoose.Schema({
 	customer_id : String,
 	menu_id : String,
 	timestamp : Date,
-	text : String,
+	text : String
 });
 
 // create the model for menu and expose it to our app
