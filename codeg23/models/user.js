a00afc3ea1eab9e5ce987170680d15da628bfc60
Yanjia Li <liyanjia92@gmail.com>
2015-04-06 19:01:45 -0400

    finish back-end for profile detail

--- codeg23/models/user.js
+++ codeg23/models/user.js
@@ -4,23 +4,47 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
-	id: String,
-	username: String,
-	password: String,
+
+	/* Demographic */
 	fullname: String,
 	firstname: String,
 	lastname: String,
+	gender: String,
+	birth_month: String,
+	birth_day: String,
+	birth_year: String,
+	languages: String,
+	nationality: String,
+
+	/* Contact */
 	email: String,
+	phone: String,
 	address: String,
+	city: String,
+	state: String,
+	country: String,
+	wechat: String,
+	linkedin: String,
+
+	/* Work and Education */
+	company: String,
+	school: String,
+
+	/* Interests and Bio */
+	interests: String,
 	bio: String,
+
+	/* Verification */
+	id: String,
+	username: String,
+	password: String,
 	customer_rate: Number,
 	owner_rate: Number,
 	edu_verified: String,
 	edu_email: String,
 	edu_key: String,
-	gender: String,
-	facebook_id: String,
-	city: String
+	facebook_id: String
+
 });
 
 // create the model for users and expose it to our app
